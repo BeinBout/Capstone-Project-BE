652c358d883cd7d2db35c025a2f3c3cdb28d55b9
AgungADL <agungcoygamez123@gmail.com>
2026-03-24 20:15:37 +0700

    fix: combine the ai helper functions into one file and then create a new file to differentiate dummy and not

--- src/controllers/profile-and-inpe.controller.js
+++ src/controllers/profile-and-inpe.controller.js
@@ -1,5 +1,5 @@
 import prisma from '../config/db.js';
-import { generateInitialPersona } from '../utils/aiHelper.js';
+import { generateInitialPersona } from '../utils/aiHelperDummy.js';
 
 export const setupProfileAndQuiz = async (req, res) => {
     try {
